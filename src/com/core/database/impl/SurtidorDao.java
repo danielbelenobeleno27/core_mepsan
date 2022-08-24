4b2ae14aa502bd7767f606f3b9a4c02cbe4495d9
danielbelenobeleno27 <desarrollogrupo12@devitech.com.co>
2022-08-24 10:00:06 -0500

    fix seteo tipoVenta

--- src/com/core/database/impl/SurtidorDao.java
+++ src/com/core/database/impl/SurtidorDao.java
@@ -2744,8 +2744,9 @@ public class SurtidorDao {
                     if (json.get("numeroDocumento") != null && !json.get("numeroDocumento").isJsonNull()) {
                         aut.setIdentificadorCLiente(json.get("numeroDocumento").getAsInt());
                     }
+                    
                     if (json.get("tipoVenta") != null && !json.get("tipoVenta").isJsonNull()) {
-                        int tipoVenta = json.get("tipoVenta").getAsInt();
+                        aut.setTipoVenta(json.get("tipoVenta").getAsInt());
                     }
                 } else {
                     aut.setIdentificadorCLiente(NeoService.CLIENTES_VARIOS_ID);
